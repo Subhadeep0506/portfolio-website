7f7c13e5ead6aef495a7aa1225ad2d327faa1177
Subhadeep Mandal <subhadeepdoublecap@gmail.com>
2024-01-11 00:05:04 +0530

    Minor changes

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,9 +1,6 @@
 import PropTypes from "prop-types";
-import { Fragment } from "react";
-import { Tooltip as ReactTooltip } from "react-tooltip";
 import "./Header.css";
 import "../../../App.css";
-import NavLinks from "../../links/NavLinks";
 
 function Header({ currentPage }) {
   if (!currentPage) {
@@ -11,16 +8,69 @@ function Header({ currentPage }) {
   }
 
   return (
-    <nav className="navbar bg-dark navbar-expand-md">
+    <nav className="navbar bg-dark navbar-expand-md navbar-dark navbar-custom">
       <h1 className="rainbow_text_animated">Subhadeep M.</h1>
-      <Fragment>
-        <NavLinks />
-      </Fragment>
-      <ReactTooltip
-        data-tooltip-id="theme-button"
-        place="bottom"
-        data-tooltip-content="Dark"
-      />
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#collapsibleNavbar"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul className="navbar-nav nav-links">
+          <li className="nav-item">
+            <a className="nav-link">
+              {/* <i className="bi bi-house-fill"></i> Home */}
+              <img src="src/assets/home.svg" className="link-icon"></img> Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link">
+              {/* <i className="bi bi-book-fill"></i> Education */}
+              <img
+                src="src/assets/education.svg"
+                className="link-icon"
+              ></img>{" "}
+              Education
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link">
+              {/* <i className="bi bi-briefcase-fill"></i> Experience */}
+              <img
+                src="src/assets/experience.svg"
+                className="link-icon"
+              ></img>{" "}
+              Experience
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link">
+              {/* <i className="bi bi-clipboard-data-fill"></i> Projects */}
+              <img
+                src="src/assets/projects.svg"
+                className="link-icon"
+              ></img>{" "}
+              Projects
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link">
+              {/* <i className="bi bi-envelope-fill"></i> Contact */}
+              <img
+                src="src/assets/contact.svg"
+                className="link-icon"
+              ></img>{" "}
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <button className="resume">Résumé</button>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
